4d9ffa45520e959eeda4434b5b438bb75bffe954
Vinay <nayakvinay58@gmail.com>
2024-08-29 22:17:24 +0530

    changes in /user.php

--- pages/user.php
+++ pages/user.php
@@ -79,7 +79,7 @@ include'../includes/sidebar.php';
 
 
 
-
+        <div>Hello world</div>
          <div class="card shadow mb-4">
             <div class="card-header py-3">
               <h4 class="m-2 font-weight-bold text-primary">User Accounts&nbsp;<a  href="#" data-toggle="modal" data-target="#supplierModal" type="button" class="btn btn-primary bg-gradient-primary" style="border-radius: 0px;"><i class="fas fa-fw fa-plus"></i></a></h4>
